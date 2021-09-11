8b20df0930bcd8f52a27c732c2f1005dd6f83860
encyclopaedier <90532823+encyclopaedier@users.noreply.github.com>
2021-09-11 18:31:19 -0400

    Add files via upload

---
+++ analysis/analysis.py
@@ -0,0 +1,9 @@
+"""
+Name : analysis.py
+Author  : Hongliang Liu
+Contect : honglian@andrew.cmu.edu
+Time    : 2021/9/11 15:59
+Desc:analyse organized data, and return users what they want
+"""
+class analysis():
+    def __init__(self):
